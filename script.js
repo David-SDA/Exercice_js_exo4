fef1388752d586dcf76fab232d97b11c50c3b8e6
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-28 10:57:31 +0200

    Ajout fonctionnalité, couleur aléatoire pour cases

--- script.js
+++ script.js
@@ -8,10 +8,21 @@ let nombreCasesCrées = 0;
 const carre = document.createElement("div");
 carre.classList.add("carre");
 
+/* Fonction pour générer une couleur aléatoire */
+function genererCouleurAleatoire(){
+    let rouge = Math.floor(Math.random() * 256);
+    let vert = Math.floor(Math.random() * 256);
+    let bleu = Math.floor(Math.random() * 256);
+    let rgb = [rouge, vert, bleu];
+    return rgb;
+}
+
 /* Création de cases */
 document.addEventListener("keyup", function(e){
     if(e.code === "ArrowDown" && nombreCasesCrées < 225){
         let nouvCarre = carre.cloneNode();
+        let couleurs = genererCouleurAleatoire();
+        nouvCarre.style.backgroundColor = "rgb(" + couleurs[0] + ", " + couleurs[1] + ", " + couleurs[2] + ")";
         contenu.appendChild(nouvCarre);
         nombreCasesCrées++;
     }
